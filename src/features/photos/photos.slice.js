3c29f8086f03014c762bd47471c7ac723acfa221
Hou Chia <kchia87@gmail.com>
2022-07-28 21:38:06 -0400

    Update photos.slice.js

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -10,8 +10,13 @@ const options = {
   name: 'photos',
   initialState,
   reducers: {
-    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos
+    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
+    // Task 1 Hint: You can use state.photos.unshift()
+    // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
+   
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
+    // Task 6 Hint: You can use state.photos.splice()
+    // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
   },
 };
 
